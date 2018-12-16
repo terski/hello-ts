22606961fa0f4b1dc06c46c1270bd2f906eb053b
Matt Terski <matt.terski@gmail.com>
2018-12-16 12:53:49 -0600

    :white_check_mark: Add tests

---
+++ src/main.test.ts
@@ -0,0 +1,9 @@
+describe('Sample test', () => {
+    it('should succeed', () => {
+        expect(true).toBeTruthy();
+    });
+
+    it('should also succeed', () => {
+        expect(2 + 3).toEqual(5);
+    });
+});
