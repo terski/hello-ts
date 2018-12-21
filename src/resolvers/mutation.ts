72d488869d240c6367a4dfbc3a37185b29b23cc8
Matt Terski <matt.terski@gmail.com>
2018-12-21 11:47:45 -0600

    Save and retrieve a basic Subscription

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -1,3 +1,12 @@
 export const Mutation = {
+    async createSubscription(parent: any, args: any, ctx: any, info: any) {
+        const subscription = await ctx.prisma.mutation.createSubscription(
+            {
+                data: { planName: args.planName, description: 'some plan' },
+            },
+            info
+        );
 
+        return subscription;
+    },
 };
--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -1,1 +1,5 @@
-export const Query = {};
+export const Query = {
+    subscriptions(parent: any, args: any, ctx: any, info: any) {
+        return ctx.prisma.query.subscriptions();
+    },
+};
